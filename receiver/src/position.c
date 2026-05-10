17f46dd38df260600d8311faf31a85c7f28f1328
spindly <kaihrmt@gmail.com>
2026-05-10 16:43:18 +0100

    Fixed positioning 0 issue
    now need to fix the jitter mess

--- receiver/src/position.c
+++ receiver/src/position.c
@@ -2,26 +2,27 @@
 #include "data_handle.h"
 #include "zephyr/kernel.h"
 #include "zephyr/sys/printk.h"
-#include <math.h> #include <stdint.h>
+#include <math.h>
+#include <stdint.h>
 
 K_MSGQ_DEFINE(positioning_queue, sizeof(struct solver_packet_t), 10, 4);
 
 // precompute this
-#define ONE_DIVIDE_THREE (float)(1 / 3)
-#define FIVE_DIVIDE_TWO (float)(5 / 2)
+static const float ONE_DIVIDE_THREE = 1.0f / 3.0f;
+static const float FIVE_DIVIDE_TWO = 5.0f / 2.0f;
 
 static struct data_container_t local_data_tx = {0};
 static struct data_container_t local_data_rx = {0};
 
 // Calibrate magnetic moment
-static float cal_magnetic_moment = 1.0f;
+static float cal_magnetic_moment = 10000.0f;
 
 void normalise_quaternion(struct quaternion_t *q) {
   // create unit quaternion
   struct quaternion_t q_in = *q;
   // get magnitude
-  float mag = (q_in.w * q_in.w) + (q_in.x * q_in.x) + (q_in.y * q_in.y) +
-              (q_in.z * q_in.z);
+  float mag = sqrtf((q_in.w * q_in.w) + (q_in.x * q_in.x) + (q_in.y * q_in.y) +
+                    (q_in.z * q_in.z));
   // normalise
   q_in.w = q_in.w / mag;
   q_in.x = q_in.x / mag;
@@ -48,7 +49,7 @@ struct quaternion_t multiply_quaternion(struct quaternion_t q1,
   return product;
 }
 
-struct vector_t rotate_vector(vector_t raw_pos, struct quaternion_t tx_q,
+struct vector_t rotate_vector(struct vector_t raw_pos, struct quaternion_t tx_q,
                               struct quaternion_t rx_q) {
   // ensure normalise the quaterion
   normalise_quaternion(&tx_q);
@@ -63,7 +64,7 @@ struct vector_t rotate_vector(vector_t raw_pos, struct quaternion_t tx_q,
   struct quaternion_t inverse_q_relative = inverse_quaternion(&q_realtive);
 
   struct quaternion_t mag_vector = {
-      .w = 0.0f, .x = rx_pos_raw.x, .y = rx_pos_raw.y, .z = rx_pos_raw.z};
+      .w = 0.0f, .x = raw_pos.x, .y = raw_pos.y, .z = raw_pos.z};
 
   // rotate magnetic vectors (rotation via shoemakers paper q^-1 * V * q)
   mag_vector = multiply_quaternion(inverse_q_relative, mag_vector);
@@ -78,6 +79,7 @@ void calculate_pos(float bx, float by, float bz) {
   // cache values so this is slightly less expensive
   // also this is entirely taken from research paper
   float sqrt_bx_by = sqrtf((bx * bx) + (by * by));
+
   float c1 = bz / sqrt_bx_by;
   float c2 = (3 * c1 / 4) + (sqrtf((9 * (c1 * c1)) + 8) / 4);
 
@@ -90,8 +92,11 @@ void calculate_pos(float bx, float by, float bz) {
   float xp = x0 / sqrtf(1 + powf((by / bx), 2.0f));
   float yp = sqrtf((x0 * x0) - (xp * xp));
 
+  // printk(
+  //     " c1: %-7.3f | c2: %-7.3f | x0: %-7.3f | x0_n: %-7.3f | x0_d:
+  //     %-7.3f\n", c1, c2, x0, x0_numerator, x0_denominator);
   // print values
-  printk(" X: %-7.3f | Y: %-7.3f | Z: %-7.3f\n", xp, yp, zp);
+  printk(" X: %-10.8f | Y: %-10.8f | Z: %-10.8f\n", xp, yp, zp);
 }
 
 void start_positioning_thread(void *, void *, void *) {
@@ -102,8 +107,16 @@ void start_positioning_thread(void *, void *, void *) {
     // data and if a position item comes in use that to stablize
     k_msgq_get(&positioning_queue, &incoming_data, K_FOREVER);
 
-    struct vector_t rx_pos_raw = {incoming_data.bx, incoming_data.by,
-                                  incoming_data.bz};
+    // struct vector_t rx_pos_raw = {incoming_data.bx, incoming_data.by,
+    //                               incoming_data.bz};
+
+    // NOTE: forced postive to see how position does, if this fixes most
+    // positoning stuff then phase sync sign needs fixing
+    struct vector_t rx_pos_raw = {
+        fabsf(incoming_data.bx), fabsf(incoming_data.by), fabsf(incoming_data.bz)};
+
+    // printk(" RAW X: %-7.3f | RAW Y: %-7.3f | RAW Z: %-7.3f\n", rx_pos_raw.x,
+    //        rx_pos_raw.y, rx_pos_raw.z);
 
     struct quaternion_t q_tx_frame = {
         .w = incoming_data.tx_q0,
@@ -120,9 +133,13 @@ void start_positioning_thread(void *, void *, void *) {
     };
 
     // rotated vector
-    struct vector_t rot_vector =
-        rotate_vector(rx_pos_raw, q_tx_frame, q_rx_frame);
+    struct vector_t rot_vector;
+    rot_vector = rotate_vector(rx_pos_raw, q_tx_frame, q_rx_frame);
+
+    // printk(" ROT X: %-7.3f | ROT Y: %-7.3f | ROT Z: %-7.3f\n", rot_vector.x,
+    //        rot_vector.y, rot_vector.z);
 
     // estimate position from rotated vector
+    calculate_pos(rot_vector.x, rot_vector.y, rot_vector.z);
   }
 }
